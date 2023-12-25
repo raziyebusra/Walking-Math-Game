0d7a2ecdc61d8a9b57e011e3c810102df009c90d
Raziye Büşra <113296996+raziyebusra@users.noreply.github.com>
2023-12-25 23:18:34 +0300

    follow player script added to camera

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class FollowPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 4, -10);
+
+    void LateUpdate()
     {
-        
+        transform.position = player.transform.position + offset;
     }
 }
