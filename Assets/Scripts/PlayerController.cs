3cbe0a047145be05face670c3cc8d64251204f6f
Raziye Büşra <113296996+raziyebusra@users.noreply.github.com>
2023-12-25 23:13:20 +0300

    PlayerMovement script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float xRange = 8f;
+    public float horizontalInput;
+    [SerializeField] float verticalSpeed = 5f;
+    [SerializeField] float horizontalSpeed = 8f;
+    public float xRange = 2.5f;
+
+    public bool isGameOver;
+
+    void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * horizontalSpeed);
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalSpeed);
+    }
 }
