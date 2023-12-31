c60cc6ebd93f743a6c242548bd72960d9b7f8015
Raziye Büşra <113296996+raziyebusra@users.noreply.github.com>
2023-12-31 16:56:34 +0300

    player smooth movement added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,73 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float sideMoveDistance = 2f; // Distance the player can move to the sides
-    public Transform groundCheck;
-    public LayerMask groundLayer;
+    public CharacterController controller;  // Reference to the character controller
+    public float speed = 5f;              // Character's forward movement speed
 
-    private CharacterController characterController;
-    private Vector3 playerVelocity;
-    private bool isGrounded;
-
-    void Start()
-    {
-        characterController = GetComponent<CharacterController>();
-    }
+    float targetXPosition = 0f;           // Variable to store the target x-position
 
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, 0.1f, groundLayer);
+        // Continuous forward movement
+        Vector3 moveDirection = transform.forward * speed * Time.deltaTime;
+        controller.Move(moveDirection);
 
-        if (isGrounded)
+        // Handle side-to-side movement with key presses
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            // Handle player movement
-            HandleSwipeInput();
-            HandleArrowKeyInput();
-
-            // Apply gravity to keep the player grounded
-            if (playerVelocity.y < 0)
-                playerVelocity.y = -2f;
-
-            // Move the player
-            characterController.Move(playerVelocity * Time.deltaTime);
+            targetXPosition = -2f;  // Set target position to -2 on x-axis
         }
-    }
-
-    void HandleSwipeInput()
-    {
-        if (Input.touchCount > 0)
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
-            Touch touch = Input.GetTouch(0);
-
-            if (touch.phase == TouchPhase.Moved)
-            {
-                // Calculate the swipe distance
-                float swipeDelta = touch.deltaPosition.x / Screen.width;
-
-                // Move the player based on swipe
-                MovePlayer(swipeDelta);
-            }
+            targetXPosition = 2f;  // Set target position to +2 on x-axis
         }
-    }
 
-    void HandleArrowKeyInput()
-    {
-        float horizontalInput = Input.GetAxis("Horizontal");
-
-        if (Mathf.Abs(horizontalInput) > 0.1f)
-        {
-            // Move the player based on arrow keys
-            MovePlayer(horizontalInput);
-        }
-    }
-
-    void MovePlayer(float input)
-    {
-        // Calculate the target position
-        float targetX = Mathf.Clamp(transform.position.x + input * sideMoveDistance, -sideMoveDistance, sideMoveDistance);
-
-        // Set the player's position directly
-        transform.position = new Vector3(targetX, transform.position.y, transform.position.z);
+        // Move towards the target position smoothly, ensuring it's within -2 and +2
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(Mathf.Clamp(targetXPosition, -2f, 2f), transform.position.y, transform.position.z), Time.deltaTime * speed * 6);
     }
 }
